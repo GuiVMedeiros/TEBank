53589542ba1e5c17104ce094d45ed68ee755a1ae
Guilherme <guilherme.victor.mix@hotmail.com>
2022-05-01 18:51:12 -0300

    Classe ContaCorrente e ContaCorrenteTeste

--- src/main/java/ContaCorrente.java
+++ src/main/java/ContaCorrente.java
@@ -1,23 +1,47 @@
 public class ContaCorrente {
 
-    private String nome;
-    private int conta, saques;
     private double saldo;
+    private double valor;
+    private String titular;
 
-    public ContaCorrente (String nome, int conta, int saques, double saldo){
-        this.nome = nome;
-        this.conta = conta;
-        this.saques = saques;
+    public ContaCorrente (double saldo, String titular){
+        this.saldo = saldo;
+        this.titular = titular;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
         this.saldo = saldo;
     }
 
     public void sacar (double valor){
-        saldo = saldo - valor;
+        if (getSaldo() >= valor) {
+            setSaldo(getSaldo() - valor);
+        } else {
+            System.out.println("Você não tem saldo suficiente para sacar!");
+        }
     }
 
     public void depositar (double valor){
-        saldo = saldo + valor;
+        setSaldo(getSaldo() + valor);
     }
-
-
 }
